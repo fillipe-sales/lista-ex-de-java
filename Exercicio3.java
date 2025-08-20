6e3d5f365edf92bbaa258ddb48ba1474db524ce0
fillipe-sales <fillipe.sales@aluno.faculdadeimpacta.com.br>
2025-08-20 20:30:33 -0300

    Add files via upload

---
+++ Exercicio3.java
@@ -0,0 +1,31 @@
+
+//Nome dos integrantes: Edino Rogério RA:2401621, Fillipe Correa RA:2403395, João Pedro Vasconcelos RA:2404081
+
+
+package Exercicio;
+
+import java.util.Scanner;
+
+public class Exercicio3 {
+
+	public static void main(String[] args) {
+		
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println("Digite seu nome: ");
+		String nome = in.nextLine();
+		
+		System.out.println("Digite o seu salário: ");
+		int salario = in.nextInt();
+		
+		
+		System.out.println("Nome: " + nome + ", salario: " + salario);
+		
+		in.close();
+					
+	}
+
+}
+
+
+
